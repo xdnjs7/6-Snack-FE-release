6420974c47ace208583b194e4a049063cd593ca5
Jam1eL1 <23jisu@gmail.com>
2025-07-14 13:07:50 +0900

    feat: DesktopHeader UI 구현완료

--- src/components/layout/GlobalHeader.tsx
+++ src/components/layout/GlobalHeader.tsx
@@ -4,12 +4,13 @@ import React from "react";
 import { useMediaQuery } from "../../hooks/useMediaQuery";
 import MobileHeader from "./MobileHeader";
 import TabletHeader from "./TabletHeader";
+import DesktopHeader from "./DesktopHeader";
 
 // 반응형에 따른 헤더 보여주기 위한 커스텀 훅 (useMediaQuery) 적용하는 부분
 const useResponsiveBreakpoints = () => {
   const isMobile = useMediaQuery("(max-width: 743px)");
-  const isTablet = useMediaQuery("(min-width: 744px) and (max-width: 1400px)");
-  const isDesktop = useMediaQuery("(min-width: 1401px)");
+  const isTablet = useMediaQuery("(min-width: 744px) and (max-width: 1399px)");
+  const isDesktop = useMediaQuery("(min-width: 1400px)");
 
   return { isMobile, isTablet, isDesktop };
 };
@@ -23,11 +24,3 @@ export default function GlobalHeader() {
 
   return <MobileHeader />;
 }
-
-const DesktopHeader = () => {
-  return (
-    <header className="hidden md:block">
-      <div>Desktop Header</div>
-    </header>
-  );
-};
