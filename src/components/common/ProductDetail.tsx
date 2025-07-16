222d165ebf3e6429b1341e84c4bed07052f6a4f1
Jam1eL1 <23jisu@gmail.com>
2025-07-17 08:22:00 +0900

    refactor: ProductDetail 한 컴포넌트 안으로 sm, md써서 분리되있던것 통합

--- src/components/common/ProductDetail.tsx
+++ src/components/common/ProductDetail.tsx
@@ -1,22 +1,131 @@
-import React from "react";
-import Mobile from "./Mobile";
-import Tablet from "./Tablet";
-import Desktop from "./Desktop";
-import MobileProductDetail from "./MobileProductDetail";
-import DesktopProductDetail from "./DesktopProductDetail";
+import React, { useState } from "react";
+import Image from "next/image";
+import ArrowIconSvg from "@/components/svg/ArrowIconSvg";
+import QuantityDropdown from "./QuantityDropdown";
+import MenuDropdown from "./MenuDropdown";
+import PlusToggleIconSvg from "@/components/svg/PlusToggleIconSvg";
+import img_coke_zero from "@/assets/images/img_coke_zero.webp";
+import ic_like_normal from "@/assets/icons/ic_like_normal.svg";
 
 export default function ProductDetail() {
+  const [selectedQuantity, setSelectedQuantity] = useState(0);
+  const [isBenefitOpen, setIsBenefitOpen] = useState(false);
+  const [isDeliveryMethodOpen, setIsDeliveryMethodOpen] = useState(false);
+  const [isDeliveryFeeOpen, setIsDeliveryFeeOpen] = useState(false);
+
   return (
-    <div>
-      <Mobile>
-        <MobileProductDetail />
-      </Mobile>
-      <Tablet>
-        <MobileProductDetail />
-      </Tablet>
-      <Desktop>
-        <DesktopProductDetail />
-      </Desktop>
+    <div className="w-full flex flex-col justify-center items-center px-6 sm:px-0 sm:max-w-[1180px]">
+      {/* 카테고리 + 이미지 wrapper */}
+      <div className="w-full flex flex-col justify-center items-start gap-7.5">
+        {/* 카테고리 */}
+        <div className="pt-3.5 pb-2.5 sm:pt-0 sm:pb-5 flex justify-start items-center gap-1 sm:gap-1.5 sm:h-16 sm:w-full sm:border-b sm:border-primary-100">
+          <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-200 tracking-tight">음료</p>
+          <div>
+            <ArrowIconSvg direction="right" className="w-3 h-3 sm:w-4 sm:h-4 text-primary-100" />
+          </div>
+          <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-950 tracking-tight">
+            청량 ∙ 탄산 음료{" "}
+          </p>
+        </div>
+
+        {/* 이미지 & 상품정보 wrapper - 데스크톱에서는 가로 배치 */}
+        <div className="w-full flex flex-col md:flex-row md:gap-10">
+          {/* 이미지 */}
+          <div className="self-center relative w-full max-w-[328px] sm:max-w-[496px] md:max-w-[540px] aspect-square">
+            <Image src={img_coke_zero} alt="코카콜라 제로" fill className="object-contain" />
+          </div>
+
+          {/* (상품 정보) (장바구니담기부터 아래 부분) wrapper  */}
+          <div className="flex flex-col justify-center md:justify-start items-center w-full gap-8 sm:gap-10 md:gap-7.5 pt-7.5 sm:pt-8 md:pt-8">
+            {/* 상품정보 wrapper */}
+            <div className="self-stretch inline-flex justify-between items-start">
+              {/* 타이틀, 구매횟수, 가격 */}
+              <div className="inline-flex flex-col justify-start items-start gap-2">
+                <div className="flex flex-col sm:flex-row justify-center items-start gap-2">
+                  <div className="text-black text-lg/[22px] font-normal tracking-tight">코카콜라 제로</div>
+                  <div className="text-secondary-500 text-sm/[17px] font-bold tracking-tight">29회 구매</div>
+                </div>
+                <div className="text-black text-lg/[22px] font-extrabold tracking-tight">2,000원</div>
+              </div>
+
+              {/* (상품수량, 드롭다운) (메뉴) */}
+              <div className="flex items-start gap-3.5 sm:gap-5">
+                <QuantityDropdown selectedQuantity={selectedQuantity} onQuantityChange={setSelectedQuantity} />
+                <MenuDropdown onEdit={() => console.log("상품 수정")} onDelete={() => console.log("상품 삭제")} />
+              </div>
+            </div>
+
+            {/* (장바구니 담기 + 좋아요 버튼), 구매혜택, 배송방법, 배송비 wrapper*/}
+            <div className="flex flex-col justify-center items-center w-full">
+              <div className="flex gap-[14px] sm:gap-4 w-full">
+                {/* 장바구니 */}
+                <div className="h-16 sm:h-18 w-full px-4 sm:px-6 py-3 sm:py-4 bg-primary-950 rounded-sm inline-flex justify-center items-center">
+                  <div className="text-center justify-center text-primary-50 text-base sm:text-lg font-bold">
+                    장바구니 담기
+                  </div>
+                </div>
+                {/* 좋아요버튼 */}
+                <div className="h-16 w-16 sm:h-18 sm:w-18 px-4 py-3 sm:py-4 bg-white rounded-sm border-1 border-gray-300 inline-flex justify-center items-center">
+                  <div className="relative w-[25px] h-[22.5px] sm:w-[28px] sm:h-[25px]">
+                    <Image src={ic_like_normal} alt="좋아요 버튼" fill className="object-contain" />
+                  </div>
+                </div>
+              </div>
+
+              {/* 구매혜택 */}
+              <div className="self-stretch py-7.5 sm:py-10 border-b border-primary-200 inline-flex flex-col justify-center items-start gap-1.5 sm:gap-2">
+                <div className="self-stretch inline-flex justify-between items-center">
+                  <div className="text-primary-950 text-base/[20px] sm:text-lg/[22px] md:text-lg font-bold tracking-tight">
+                    구매혜택
+                  </div>
+                  <PlusToggleIconSvg isOpen={isBenefitOpen} onClick={() => setIsBenefitOpen(!isBenefitOpen)} />
+                </div>
+                {isBenefitOpen && (
+                  <div className="text-primary-600 text-sm sm:text-base/[22px] md:text-base font-normal tracking-tight">
+                    5포인트 적립 예정
+                  </div>
+                )}
+              </div>
+
+              {/* 배송방법 */}
+              <div className="self-stretch py-7.5 sm:py-10 border-b border-primary-200 inline-flex flex-col justify-center items-start gap-1.5 sm:gap-2">
+                <div className="self-stretch inline-flex justify-between items-center">
+                  <div className="text-primary-950 text-base/[20px] sm:text-lg/[22px] md:text-lg font-bold tracking-tight">
+                    배송 방법
+                  </div>
+                  <PlusToggleIconSvg
+                    isOpen={isDeliveryMethodOpen}
+                    onClick={() => setIsDeliveryMethodOpen(!isDeliveryMethodOpen)}
+                  />
+                </div>
+                {isDeliveryMethodOpen && (
+                  <div className="text-primary-600 text-sm sm:text-base/[22px] md:text-base font-normal tracking-tight">
+                    택배
+                  </div>
+                )}
+              </div>
+
+              {/* 배송비 */}
+              <div className="self-stretch py-7.5 sm:py-10 border-b border-primary-200 inline-flex flex-col justify-center items-start gap-1.5 sm:gap-2">
+                <div className="self-stretch inline-flex justify-between items-center">
+                  <div className="text-primary-950 text-base/[20px] sm:text-lg/[22px] md:text-lg font-bold tracking-tight">
+                    배송비
+                  </div>
+                  <PlusToggleIconSvg
+                    isOpen={isDeliveryFeeOpen}
+                    onClick={() => setIsDeliveryFeeOpen(!isDeliveryFeeOpen)}
+                  />
+                </div>
+                {isDeliveryFeeOpen && (
+                  <div className="text-primary-600 text-sm sm:text-base/[20px] md:text-base font-normal tracking-tight">
+                    3,000원 (50,000원 이상 무료배송) <span className="text-primary-400">도서산간 배송비 추가</span>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
