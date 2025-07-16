6d148b0b34c81c49731fea6a4436cd571fdf9c1d
Jam1eL1 <23jisu@gmail.com>
2025-07-17 00:16:08 +0900

    feat: 태블릿/데스크탑 버전 반응형 구현완료, ! 아이콘 컴포넌트생성

--- src/components/common/DeleteAccountConfirmModal.tsx
+++ src/components/common/DeleteAccountConfirmModal.tsx
@@ -1,19 +1,32 @@
 import React from "react";
 import Button from "../ui/Button";
+import ExclamationMarkIconSvg from "../svg/ExclamationMarkIconSvg";
 
 export default function DeleteAccountConfirmModal() {
   return (
     <div>
-      <div className="w-[327px] px-7 pt-10 pb-7 bg-white rounded-md shadow-lg inline-flex flex-col justify-center items-center gap-9">
+      <div className="w-[327px] sm:w-[512px] px-7.5 pt-10 pb-7.5 bg-white rounded-md shadow-lg inline-flex flex-col justify-center items-center gap-9">
         <div className="flex flex-col justify-start items-center gap-2">
           <div className="justify-center text-black text-lg font-bold">계정 탈퇴</div>
-          <div className="text-center justify-center">
-            <span className="text-gray-900 text-base font-bold leading-relaxed">김스낵(sn@codeit.com)</span>
-            <span className="text-gray-900 text-base font-normal leading-relaxed">
-              님의
-              <br />
-              계정을 탈퇴시킬까요?
-            </span>
+          <div className="flex flex-col justify-start items-center gap-2">
+            <ExclamationMarkIconSvg className="hidden sm:block text-red" />
+            <div className="text-center justify-center">
+              {/* 모바일 버전 */}
+              <div className="block sm:hidden">
+                <span className="text-gray-900 text-base font-bold leading-relaxed">김스낵(sn@codeit.com)</span>
+                <span className="text-gray-900 text-base font-normal leading-relaxed">
+                  님의
+                  <br />
+                  계정을 탈퇴시킬까요?
+                </span>
+              </div>
+
+              {/* 데스크톱 버전  */}
+              <div className="hidden sm:block">
+                <span className="text-gray-900 text-base font-bold leading-relaxed">김스낵(sn@codeit.com)</span>
+                <span className="text-gray-900 text-base font-normal leading-relaxed">님의 계정을 탈퇴시킬까요?</span>
+              </div>
+            </div>
           </div>
         </div>
         <div className="self-stretch inline-flex justify-start items-center gap-2.5">
@@ -21,12 +34,12 @@ export default function DeleteAccountConfirmModal() {
           <Button
             type="white"
             label="더 생각해볼게요"
-            className="h-[50px] flex-1 text-sm/[17px] font-bold tracking-tight"
+            className="h-[50px] sm:h-[64px] flex-1 text-sm/[17px] sm:text-base/[20px] font-bold tracking-tight"
           />
           <Button
             type="black"
             label="탈퇴시키기"
-            className="bg-primary-950 h-[50px] flex-1 text-sm/[17px] font-bold tracking-tight"
+            className="bg-primary-950 h-[50px] sm:h-[64px] flex-1 text-sm/[17px] sm:text-base/[20px] font-bold tracking-tight"
           />
         </div>
       </div>
