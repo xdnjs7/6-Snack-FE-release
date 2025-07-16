9b409c6c5db9ed3ed2becd9f7066f846ece595d9
Jam1eL1 <23jisu@gmail.com>
2025-07-17 08:37:55 +0900

    refactor: ProductDetail 가독성 위해 코드 분리

---
+++ src/components/common/ProductDetail/ProductActions.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import QuantityDropdown from "../QuantityDropdown";
+import MenuDropdown from "../MenuDropdown";
+
+type ProductActionsProps = {
+  selectedQuantity: number;
+  onQuantityChange: (quantity: number) => void;
+};
+
+export default function ProductActions({ selectedQuantity, onQuantityChange }: ProductActionsProps) {
+  return (
+    <div className="flex items-start gap-3.5 sm:gap-5">
+      <QuantityDropdown selectedQuantity={selectedQuantity} onQuantityChange={onQuantityChange} />
+      <MenuDropdown onEdit={() => console.log("상품 수정")} onDelete={() => console.log("상품 삭제")} />
+    </div>
+  );
+}
