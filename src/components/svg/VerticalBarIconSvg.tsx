ef8c8c132d325eb316e145119c676df96b079e7d
Jam1eL1 <23jisu@gmail.com>
2025-07-18 08:52:56 +0900

    chore: VerticalBarIconSvg  팀 컨벤션에 맞게 수정

--- src/components/svg/VerticalBarIconSvg.tsx
+++ src/components/svg/VerticalBarIconSvg.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-interface VerticalBarIconSvgProps {
+type TVerticalBarIconSvgProps = {
   className?: string;
-}
+};
 
-export default function VerticalBarIconSvg({ className = "" }: VerticalBarIconSvgProps) {
+export default function VerticalBarIconSvg({ className = "" }: TVerticalBarIconSvgProps) {
   return (
     <svg width="1" height="11" viewBox="0 0 1 11" fill="none" xmlns="http://www.w3.org/2000/svg" className={className}>
       <rect width="1" height="10" transform="translate(0 0.5)" fill="currentColor" />
