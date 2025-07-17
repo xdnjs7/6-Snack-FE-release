f4a7843ff98ee5fab963703a6469ab0fd70db934
Jam1eL1 <23jisu@gmail.com>
2025-07-17 17:05:21 +0900

    feat: ProfileAvatar 컴포넌트 추가, 유저 성을 프롭스로 받아옴

---
+++ src/components/common/ProfileAvatar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ProfileAvatarProps {
+  label: string;
+  className?: string;
+}
+
+export default function ProfileAvatar({ label, className = "" }: ProfileAvatarProps) {
+  return (
+    <div
+      className={`w-6 h-6 bg-primary-100 py-1.5 px-[7.5] rounded-[100px] flex items-center justify-center overflow-hidden ${className}`}
+    >
+      <div className=" text-primary-950 text-[10px] tracking-tight font-medium">{label}</div>
+    </div>
+  );
+}
