dc5435a5936863ddd775aa29ca061fc5f14e9bf5
Decal <vumublne@naver.com>
2025-07-17 18:57:02 +0900

    chore: 배포를 위한 임시 커밋

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import img_logo from "@/assets/images/img_logo.webp";
 
 export default function LoginPage() {
   return (
-    <div>page</div>
-  )
+    <div>
+      <div className="relative w-[327px] h-[140px]">
+        <Image src={img_logo} alt="Snack 로고" fill className="object-cover" />
+      </div>
+    </div>
+  );
 }
