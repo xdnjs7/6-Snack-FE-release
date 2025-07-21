c147ae08d30f12e201b185e04029b45270892e3f
Jam1eL1 <23jisu@gmail.com>
2025-07-21 11:38:28 +0900

    feat: 임시 동적페이지 auth/signup/[inviteId] 생성

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,18 +2,59 @@
 import MobileCategoryMenu from "@/components/common/MobileCategoryMenu";
 import SubCategoryItem from "@/components/common/SubCategoryItem";
 import AuthenticatedHeader from "@/components/layout/AuthenticatedHeader";
+import Image from "next/image";
 import React from "react";
+import img_logo from "@/assets/images/img_logo.webp";
+import SnackIconSvg from "@/components/svg/SnackIconSvg";
 
 export default function SignUpPage() {
+  // AuthProvider 사용 예시:
+  // import { useAuth } from "@/providers/AuthProvider";
+  // const { user, isLoading, login, logout, updateUser } = useAuth();
+  //
+  // if (isLoading) return <div>Loading...</div>;
+  // if (!user) return <div>로그인이 필요합니다</div>;
+  //
+  // UserResponse {
+  // user: {
+  //   id: string;
+  //   email: string;
+  //   name: string;
+  //   role: string;
+  //   cartItemCount: number; // 추가
+  //   company: {
+  //     id: number;
+  //     name: string;
+  //   };
+  // }
+
+  // TODO: getInviteApi 구현후 대체해야함
+  const user = {
+    id: "temp-user-123",
+    email: "test@example.com",
+    name: "김철수",
+    role: "user",
+    cartItemCount: 0,
+    company: {
+      id: 1,
+      name: "테스트 회사",
+    },
+  };
   return (
-    <div>
-      <AuthenticatedHeader />
-      <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M18.8936 9.34839L12 16.2429L5.10547 9.34839L6.16602 8.28784L11.999 14.1208L17.833 8.28784L18.8936 9.34839Z"
-          fill="black"
-        />
-      </svg>
+    // top parent
+    <div className="flex flex-col items-center justify-center px-[24px] pt-[60px] gap-[46px] sm:px-[72px] sm:pt-[119px] sm:gap-0">
+      {/* mobile */}
+      {/* logo + intro */}
+      <div className="flex flex-col items-center justify-center w-full">
+        <div className="px-[60px] py-[38px]">
+          <SnackIconSvg className="w-[225px] h-[63px] sm:w-[344px] sm:h-[97px]" />
+        </div>
+        <h1>{user.name} 님, 만나서 반갑습니다.</h1>
+        <p>비밀번호를 입력해 회원가입을 완료해주세요.</p>
+      </div>
+      {/* signup content */}
+      <div className="flex flex-col w-full"></div>
+      <div></div>
     </div>
   );
 }
