8f11cb0bc051648be07b071222f5d387de8376be
Jam1eL1 <23jisu@gmail.com>
2025-07-25 06:35:39 +0900

    refactor: CategoryNavigation 부모에서 className 프롭스 받도록 수정

--- src/components/common/ProductDetail/CategoryNavigation.tsx
+++ src/components/common/ProductDetail/CategoryNavigation.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import ArrowIconSvg from "@/components/svg/ArrowIconSvg";
 
-interface CategoryNavigationProps {
+type CategoryNavigationProps = {
   parentCategory?: string;
   childCategory?: string;
-}
+  className?: string;
+};
 
-export default function CategoryNavigation({ parentCategory, childCategory }: CategoryNavigationProps) {
+export default function CategoryNavigation({ parentCategory, childCategory, className }: CategoryNavigationProps) {
   // 카테고리가 선택되지 않았으면 렌더링하지 않음
   if (!parentCategory || !childCategory) {
     return null;
   }
 
   return (
-    <div className="pt-3.5 pb-2.5 sm:pt-0 sm:pb-5 flex justify-start items-center gap-1 sm:gap-1.5 sm:h-16 sm:w-full sm:border-b sm:border-primary-100">
-      <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-200 tracking-tight">{parentCategory}</p>
-      <div>
-        <ArrowIconSvg direction="right" className="w-3 h-3 sm:w-4 sm:h-4 text-primary-100" />
+    <div className={className}>
+      <div className="pt-3.5 pb-2.5 sm:pt-0 sm:pb-5 flex justify-start items-center gap-1 sm:gap-1.5 sm:h-16 sm:w-full sm:border-b sm:border-primary-100">
+        <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-200 tracking-tight">
+          {parentCategory}
+        </p>
+        <div>
+          <ArrowIconSvg direction="right" className="w-3 h-3 sm:w-4 sm:h-4 text-primary-100" />
+        </div>
+        <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-950 tracking-tight">
+          {childCategory}
+        </p>
       </div>
-      <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-950 tracking-tight">{childCategory}</p>
     </div>
   );
 }
