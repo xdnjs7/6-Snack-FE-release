07d1f3f24c1e1e6efe544c81eab6021849e9380d
Jam1eL1 <23jisu@gmail.com>
2025-07-25 07:11:40 +0900

    refactor: CategoryNavigation 부모에서 className 적용시켰을때 변하는 위치 변경, twMerge & clsx 적용

--- src/components/common/ProductDetail/CategoryNavigation.tsx
+++ src/components/common/ProductDetail/CategoryNavigation.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ArrowIconSvg from "@/components/svg/ArrowIconSvg";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
 
 type CategoryNavigationProps = {
   parentCategory?: string;
@@ -14,18 +16,19 @@ export default function CategoryNavigation({ parentCategory, childCategory, clas
   }
 
   return (
-    <div className={className}>
-      <div className="pt-3.5 pb-2.5 sm:pt-0 sm:pb-5 flex justify-start items-center gap-1 sm:gap-1.5 sm:h-16 sm:w-full sm:border-b sm:border-primary-100">
-        <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-200 tracking-tight">
-          {parentCategory}
-        </p>
-        <div>
-          <ArrowIconSvg direction="right" className="w-3 h-3 sm:w-4 sm:h-4 text-primary-100" />
-        </div>
-        <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-950 tracking-tight">
-          {childCategory}
-        </p>
+    <div
+      className={twMerge(
+        clsx(
+          "pt-3.5 pb-2.5 sm:pt-0 sm:pb-5 flex justify-start items-center gap-1 sm:gap-1.5 sm:h-16 sm:w-full sm:border-b sm:border-primary-100",
+        ),
+        className,
+      )}
+    >
+      <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-200 tracking-tight">{parentCategory}</p>
+      <div>
+        <ArrowIconSvg direction="right" className="w-3 h-3 sm:w-4 sm:h-4 text-primary-100" />
       </div>
+      <p className="font-normal text-sm/[17px] sm:text-base/[20px] text-primary-950 tracking-tight">{childCategory}</p>
     </div>
   );
 }
