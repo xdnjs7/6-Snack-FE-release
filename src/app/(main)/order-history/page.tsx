2eab6c2e3487162e69c97f484a4a6215b6ec167e
rakaso598 <nexonaddress98@gmail.com>
2025-07-27 18:12:58 +0900

    refactor: (즉시 요청) 뱃지 다듬기

--- src/app/(main)/order-history/page.tsx
+++ src/app/(main)/order-history/page.tsx
@@ -549,11 +549,11 @@ const OrderHistoryPage = () => {
                 currentItems.map((item) => (
                   <div key={item.id} className="self-stretch h-24 px-10 border-b border-neutral-200 inline-flex justify-between items-center">
                     <div className="w-32 justify-start text-neutral-800 text-base font-normal font-['SUIT']">{item.requestDate}</div>
-                    <div className="w-32 flex justify-start items-center gap-2">
+                    <div className="w-40 flex justify-start items-center gap-2">
                       <div className="justify-start text-neutral-800 text-base font-normal font-['SUIT']">{item.requester}</div>
                       {item.adminMessage?.includes("즉시 구매") && (
                         <div className="px-2 py-1 bg-blue-50 rounded-[100px] flex justify-center items-center gap-1">
-                          <div className="justify-center text-blue-500 text-xs font-bold font-['SUIT']">즉시 요청</div>
+                          <div className="justify-center items-center text-center text-blue-500 text-xs font-bold font-['SUIT'] w-12">즉시 요청</div>
                         </div>
                       )}
                     </div>
