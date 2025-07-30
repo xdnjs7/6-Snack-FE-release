5850bef4f9bcef960cff234863a466de890fee5f
rakaso598 <nexonaddress98@gmail.com>
2025-07-30 10:49:45 +0900

    fix: next.config에서 빌드 시 설정 주석친 것 해제

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // eslint: {
-  //   ignoreDuringBuilds: true, // 빌드 시 ESLint 무시
-  // },
-  // typescript: {
-  //   ignoreBuildErrors: true, // TypeScript 오류도 무시하려면
-  // },
+  eslint: {
+    ignoreDuringBuilds: true, // 빌드 시 ESLint 무시
+  },
+  typescript: {
+    ignoreBuildErrors: true, // TypeScript 오류도 무시하려면
+  },
   images: {
     remotePatterns: [{ hostname: "example.com" }, { hostname: "team3-snack-s3.s3.amazonaws.com" }],
   },
