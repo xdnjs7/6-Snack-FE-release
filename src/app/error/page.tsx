4f9426a2337131d512f3632953e31d710f6be2a5
Decal <vumublne@naver.com>
2025-08-08 02:15:56 +0900

    fix: 안쓰는 타입 제거

--- src/app/error/page.tsx
+++ src/app/error/page.tsx
@@ -3,8 +3,6 @@
 import React from "react";
 import ErrorPageContent from "./_components/ErrorPageContent";
 
-type TRole = "USER" | "ADMIN";
-
 export default async function ErrorPage({ searchParams }: { searchParams: Promise<{ from?: string }> }) {
   const params = await searchParams;
 
