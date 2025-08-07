c6b6803c96472ab1555b0849fee9db117084ec20
Wooju <dnwn810@naver.com>
2025-08-07 15:28:31 +0900

    fix:프로필 w값 수정

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -2,7 +2,7 @@ import ProfileForm from "./_components/ProfileForm";
 
 function Page() {
   return (
-    <div className="pt-[77px] sm:pt-[110px] flex justify-center bg-white">
+    <div className="w-full pt-[77px] sm:pt-[110px] flex sm:justify-center bg-white ">
       <ProfileForm />
     </div>
   );
