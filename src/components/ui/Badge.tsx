62d23d012b3d00225a0806d5096ac5ce189e5e05
Wooju <dnwn810@naver.com>
2025-08-14 01:07:11 +0900

    feat: 시계 svg로 추가했습니다

--- src/components/ui/Badge.tsx
+++ src/components/ui/Badge.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import Image, { StaticImageData } from "next/image";
 import XIconSvg from "@/components/svg/XIconSvg";
-import IcPending from "@/assets/icons/ic_time.svg";
+import TimeIconSvg from "@/components/svg/TimeIconSvg";
 import { TBadgeProps, TBadgeType } from "@/types/badge.types";
 import CheckIconSvg from "@/components/svg/CheckIconSvg";
 
@@ -24,7 +24,6 @@ const badgeStyleMap: Record<
     bg: "bg-primary-200",
     text: "대기 중",
     textColor: "text-primary-700",
-    iconSrc: IcPending,
   },
   approved: {
     bg: "bg-sky-100",
@@ -70,6 +69,10 @@ export default function Badge({ type }: TBadgeProps) {
       ) : type === "approved" ? (
         <div className="w-3.5 h-3.5">
           <CheckIconSvg stroke="white" bgColor="#0EA5E9" className="w-full h-full" />
+        </div>
+      ) : type === "pending" ? (
+        <div className="w-3.5 h-3.5">
+          <TimeIconSvg className="w-3 h-3 sm:w-3.5 sm:h-3.5 text-primary-700" />
         </div>
       ) : iconSrc ? (
         <div className="w-3.5 h-3.5 relative">
