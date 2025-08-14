3f58d0b0fd44baadbc9b97444b4d6410bac8e5d0
Decal <vumublne@naver.com>
2025-08-14 09:13:57 +0900

    refactor: NoContent 컴포넌트 props 수정

--- src/components/common/NoContent.tsx
+++ src/components/common/NoContent.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Image from "next/image";
 import ic_no_order from "@/assets/icons/ic_no_order.svg";
+import { twMerge } from "tailwind-merge";
 
 type TNoContentProps = {
   title: string;
   subText1?: string;
   subText2?: string;
   buttonText: string;
+  className?: string;
   onClick: () => void;
 };
 
@@ -15,11 +17,17 @@ export default function NoContent({
   subText1 = "설명1",
   subText2 = "설명2",
   buttonText,
+  className,
   onClick,
 }: TNoContentProps) {
   return (
     <section className="flex flex-1 justify-center" role="status" aria-label="빈 상태">
-      <div className="sm:w-80 inline-flex flex-col justify-start items-center gap-7 pt-12 mt-[58px] sm:mt-[222px] md:mt-[191px]">
+      <div
+        className={twMerge(
+          "sm:w-80 inline-flex flex-col justify-start items-center gap-7 pt-12 mt-[58px] sm:mt-[222px] md:mt-[191px]",
+          className,
+        )}
+      >
         <div className="w-24 h-24 relative" role="img" aria-label="주문 내역 없음 아이콘">
           <Image src={ic_no_order} alt="주문 내역 없음" fill className="object-contain" />
         </div>
